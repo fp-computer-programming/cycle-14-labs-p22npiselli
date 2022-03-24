5d9d5ed5611a4565caa8519724abb323cb203fba
Nolan Piselli <p22npiselli@fairfieldprep.org>
2022-03-23 21:25:40 -0400

    LAB 14

---
+++ lab_14-1.py
@@ -0,0 +1,25 @@
+# Nolan Piselli
+
+# Import turtle
+import turtle
+
+# Creates window
+window = turtle.Screen()
+window.setup(1000, 1000)
+window.screensize(100,100)
+window.title("Lab 1")
+
+# Sets turtle name
+pierre = turtle.Turtle()
+
+# Directs turtle
+pierre.forward(250)
+pierre.right(90)
+pierre.forward(100)
+pierre.right(90)
+pierre.forward(250)
+pierre.right(90)
+pierre.forward(100)
+
+# Keeps window open
+window.mainloop()
